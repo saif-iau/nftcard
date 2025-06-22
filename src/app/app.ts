19a00e7bd9555b60780233dc7ed6b9744ca9b58c
saif-iau <saifalshiban@gmail.com>
2025-06-22 20:59:56 +0300

    nft project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'nftproject';
+   constructor(private el: ElementRef) {}
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const card = this.el.nativeElement.querySelector('.card');
+    if (!card) return;
+
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * 10;
+    const rotateY = ((x - centerX) / centerX) * -10;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    const card = this.el.nativeElement.querySelector('.card');
+    if (!card) return;
+
+    card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  }
+
 }
